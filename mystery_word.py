45ebbe179842fad4b1ffd960c1298185a6338093
destanton <delisestanton@gmail.com>
2016-09-15 17:57:18 -0400

    added while loop

--- mystery_word.py
+++ mystery_word.py
@@ -1,8 +1,10 @@
 import random
+import sys
 with open("/usr/share/dict/words") as lines:
     lines = lines.readlines()
-comp_word = random.choice(lines)
-# At the start of the game, let the user know how many letters the computer's word contains.
+comp_word = random.choice(lines).lower().replace("\n", "")
+print(comp_word)
+#  At the start of the game, let the user know how many letters the computer's word contains.
 comp_letters = len(comp_word)
 print(comp_letters)
 # Ask the user to supply one guess (i.e. letter) per round. This letter can be upper or lower case and it should not matter. Assume the user will only submit one letter
@@ -10,27 +12,39 @@ good_guesses = []
 bad_guesses = []
 letter_list = 0
 print(comp_word)
-print("You have 8 chances to guess my word!")
+print("Welcome to Mystery Word! You have 8 chances to guess my word! My word has {} letters in it".format(len(comp_word)))
 
-
+blank_word = list("_" * comp_letters)
 # Let the user know if their guess appears in the computer's word
 # A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
 # If the user guesses the same letter twice, do not take away a guess. Instead, print a message letting them know they've already guessed that letter and ask them to try again.
 while letter_list < 8:
     guess = input("Guess a letter: ").lower()
     if guess in comp_word:
+        if guess in good_guesses:
+            print("You've already guessed that letter! Please try again!")
         good_guesses.append(guess)
+        for current_location, current_letter in enumerate(comp_word):
+            if guess == current_letter:
+                blank_word[current_location] = guess
         print("That guess is in the computer word! You have {}/8 guesses".format(letter_list))
-    elif guess in good_guesses:
-        print("You've already guessed that letter!")
-        print(good_guesses)
-    elif guess != comp_word:
+        print(*blank_word)
+        #print(list(comp_word), blank_word, "DANIELLE!!!!")
+        # print(good_guesses)
+        if blank_word == list(comp_word):
+            print ("You win!")
+            sys.exit()
+    # if guess in bad_guesses:
+    #     print("You've already guessed that letter!")
+        # print(bad_guesses, "DANIELLE")
+    else:
         bad_guesses.append(guess)
         letter_list += 1
         print("That guess is not in the computer's word! You have {}/8 guesses".format(letter_list))
-        print(bad_guesses)
-    else:
-        break
+
+        # print(bad_guesses, "DANIELLE")
+
+
 
 
 # Display the partially guessed word, as well as letters that have not been guessed.
